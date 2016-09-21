26c0cdbda8f66d0b773d090f7263421273b10452
Kai Curry <kai@cape.io>
2016-09-20 21:11:29 -0500

    v1.0.1 fix selectAuth

--- src/select.js
+++ src/select.js
@@ -1,8 +1,8 @@
 import negate from 'lodash/negate'
-
+import property from 'lodash/property'
 import { select } from 'cape-select'
 
-export const selectAuth = select('auth')
+export const selectAuth = property('auth')
 export const selectUser = select('user', selectAuth)
 export const isAuthenticated = select('authenticated', selectAuth)
 export const isAnonymous = negate(isAuthenticated)
